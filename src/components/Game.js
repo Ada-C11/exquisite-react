05ea3f0a58ec4c833f0c9d9c2679fbc6b5817ad7
Amy Phung <phung.amy85@gmail.com>
2019-06-18 14:52:59 -0700

    game receives new lines from player submission form

--- src/components/Game.js
+++ src/components/Game.js
@@ -8,6 +8,17 @@ class Game extends Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      submissions: [],
+    }
+  }
+
+  addNewLine = (newLine) => {
+    const submissions = this.state.submissions;
+    submissions.push(newLine);
+
+    this.setState({ submissions })
   }
 
   render() {
@@ -29,12 +40,12 @@ class Game extends Component {
         <p>Please follow the following format for your poetry submission:</p>
 
         <p className="Game__format-example">
-          { exampleFormat }
+          {exampleFormat}
         </p>
 
         <RecentSubmission />
 
-        <PlayerSubmissionForm />
+        <PlayerSubmissionForm addNewLineCallback={this.addNewLine} />
 
         <FinalPoem />
 
--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -5,6 +5,46 @@ class PlayerSubmissionForm extends Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      adjective1: "",
+      noun1: "",
+      adverb: "",
+      verb: "",
+      adjective2: "",
+      noun2: "",
+    };
+  }
+
+  onValueChange = (event) => {
+    console.log(`${event.target} Updated ${event.target.value}`);
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newLine = {
+      adjective1: this.state.adjective1,
+      noun1: this.state.noun1,
+      adverb: this.state.adverb,
+      verb: this.state.verb,
+      adjective2: this.state.adjective2,
+      noun2: this.state.noun2,
+    }
+
+    this.setState({
+      adjective1: "",
+      noun1: "",
+      adverb: "",
+      verb: "",
+      adjective2: "",
+      noun2: "",
+    });
+
+    this.props.addNewLineCallback(newLine);
   }
 
   render() {
@@ -13,22 +53,70 @@ class PlayerSubmissionForm extends Component {
       <div className="PlayerSubmissionForm">
         <h3>Player Submission Form for Player #{}</h3>
 
-        <form className="PlayerSubmissionForm__form" >
+        <form
+          className="PlayerSubmissionForm__form"
+          onSubmit={this.onFormSubmit} >
 
           <div className="PlayerSubmissionForm__poem-inputs">
             The
-            <input placeholder="adjective" type="text" class="PlayerSubmissionForm__input--invalid" value="" />
-            <input placeholder="noun" type="text" class="PlayerSubmissionForm__input--invalid" value="" />
-            <input placeholder="adverb" type="text" class="PlayerSubmissionForm__input--invalid" value="" />
-            <input placeholder="verb" type="text" class="PlayerSubmissionForm__input--invalid" value="" />
+            <input
+              name="adjective1"
+              placeholder="adjective"
+              type="text"
+              className="PlayerSubmissionForm__poem-inputs"
+              onChange={this.onValueChange}
+              value={this.state.adjective1}
+            />
+            <input
+              name="noun1"
+              placeholder="noun"
+              type="text"
+              className="PlayerSubmissionForm__poem-inputs"
+              onChange={this.onValueChange}
+              value={this.state.noun1}
+            />
+            <input
+              name="adverb"
+              placeholder="adverb"
+              type="text"
+              className="PlayerSubmissionForm__poem-inputs"
+              onChange={this.onValueChange}
+              value={this.state.adverb}
+            />
+            <input
+              name="verb"
+              placeholder="verb"
+              type="text"
+              className="PlayerSubmissionForm__poem-inputs"
+              onChange={this.onValueChange}
+              value={this.state.verb}
+            />
             the
-            <input placeholder="adjective" type="text" class="PlayerSubmissionForm__input--invalid" value="" />
-            <input placeholder="noun" type="text" class="PlayerSubmissionForm__input--invalid" value="" />
+            <input
+              name="adjective2"
+              placeholder="adjective"
+              type="text"
+              className="PlayerSubmissionForm__poem-inputs"
+              onChange={this.onValueChange}
+              value={this.state.adjective2}
+            />
+            <input
+              name="noun2"
+              placeholder="noun"
+              type="text"
+              className="PlayerSubmissionForm__poem-inputs"
+              onChange={this.onValueChange}
+              value={this.state.noun2}
+            />
             .
           </div>
-
           <div className="PlayerSubmissionForm__submit">
-            <input type="submit" value="Submit Line" className="PlayerSubmissionForm__submit-btn" />
+            <input
+              name="submit"
+              type="submit"
+              className="PlayerSubmissionForm__submit-btn"
+              value="Submit Line"
+            />
           </div>
         </form>
       </div>

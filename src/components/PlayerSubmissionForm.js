205bca86250daf0a3f8ab183699940676b0ce5f4
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-06-18 14:49:59 -0700

    Added functionality for submitting a line of the poem

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -5,25 +5,75 @@ class PlayerSubmissionForm extends Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      adjectiveOne: "adjective",
+      nounOne: "noun",
+      adverb: "adverb",
+      verb: "verb",
+      adjectiveTwo: "adjective",
+      nounTwo: "noun",
+    }
+  }
+
+  ChangeHandler = () => {
+
+  }
+
+  onClickSubmit = (event) => {
+    event.preventDefault()
+
+    this.props.onSubmitLineCallback(this.state)
+    
+    // will probably need to clear the state after this when not hard coded!!
   }
 
   render() {
+    const { onSubmitLineCallback, playerNum } = this.props
 
     return (
       <div className="PlayerSubmissionForm">
-        <h3>Player Submission Form for Player #{  }</h3>
+        <h3>Player Submission Form for Player #{playerNum}</h3>
 
-        <form className="PlayerSubmissionForm__form" >
+        <form onSubmit={this.onClickSubmit} className="PlayerSubmissionForm__form" >
 
           <div className="PlayerSubmissionForm__poem-inputs">
-
-            {
-              // Put your form inputs here... We've put in one below as an example
-            }
             <input
-              placeholder="hm..."
-              type="text" />
-
+              name='adjectiveOne'
+              placeholder="adjective"
+              type="text"
+              onChange={this.changeHandler}
+              value="adjective" />
+            <input
+              name='nounOne'
+              placeholder="noun"
+              type="text" 
+              onChange={this.changeHandler}
+              value="noun"/>
+            <input
+              name='adverb'
+              placeholder="adverb"
+              type="text" 
+              onChange={this.changeHandler}
+              value="adverb"/>
+            <input
+              name='verb'
+              placeholder="verb"
+              type="text" 
+              onChange={this.changeHandler}
+              value="verb"/>
+            <input
+              name='adjectiveTwo'
+              placeholder="adjective"
+              type="text" 
+              onChange={this.changeHandler}
+              value="adjective"/>
+            <input
+              name='nounTwo'
+              placeholder="noun"
+              type="text" 
+              onChange={this.changeHandler}
+              value="noun"/>
           </div>
 
           <div className="PlayerSubmissionForm__submit">

a203dfea443203973f624e81122414f4ca438265
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-06-18 16:21:22 -0700

    updated player submissions fomr hard coded data to user input

--- src/components/FinalPoem.js
+++ src/components/FinalPoem.js
@@ -5,21 +5,28 @@ const FinalPoem = (props) => {
 
   const condition = props.submissions.length > 0
 
+  const submissions = props.submissions.map((line) => {
+    return (
+      <p>{line}</p>
+    );
+  });
+
   return (
     <div className="FinalPoem">
+      
       <section className="FinalPoem__poem">
         <h3>Final Poem</h3>
-
+        { 
+          condition && (
+            <div>{submissions}</div>
+          )
+        }
       </section>
 
       <div className="FinalPoem__reveal-btn-container">
         <input type="button" value="We are finished: Reveal the Poem" className="FinalPoem__reveal-btn" />
-        { 
-          condition && (
-            <section>{props.submissions}</section>
-          )
-        }
       </div>
+
     </div>
   );
 }
--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -7,17 +7,21 @@ class PlayerSubmissionForm extends Component {
     super(props);
 
     this.state = {
-      adjectiveOne: "adjective",
-      nounOne: "noun",
-      adverb: "adverb",
-      verb: "verb",
-      adjectiveTwo: "adjective",
-      nounTwo: "noun",
+      adjectiveOne: "",
+      nounOne: "",
+      adverb: "",
+      verb: "",
+      adjectiveTwo: "",
+      nounTwo: "",
     }
   }
 
-  ChangeHandler = () => {
+  onChangeHandler = (event) => {
+    console.log(event.target.name);
+    const field = {}
+    field[event.target.name] = event.target.value
 
+    this.setState(field)
   }
 
   onClickSubmit = (event) => {
@@ -25,55 +29,64 @@ class PlayerSubmissionForm extends Component {
 
     this.props.onSubmitLineCallback(this.state)
     
+    const emptyState = this.state
+
+    Object.keys(emptyState).forEach((key) => {
+      emptyState[key] = '';
+    })
+
+    this.setState(emptyState);
     // will probably need to clear the state after this when not hard coded!!
   }
 
   render() {
     const { onSubmitLineCallback, playerNum } = this.props
+    const { adjectiveOne, nounOne, adverb, verb, adjectiveTwo, nounTwo } = this.state
 
     return (
       <div className="PlayerSubmissionForm">
         <h3>Player Submission Form for Player #{playerNum}</h3>
 
-        <form onSubmit={this.onClickSubmit} className="PlayerSubmissionForm__form" >
-
+        <form 
+          onSubmit={this.onClickSubmit} 
+          className="PlayerSubmissionForm__form" >
           <div className="PlayerSubmissionForm__poem-inputs">
             <input
               name='adjectiveOne'
               placeholder="adjective"
               type="text"
-              onChange={this.changeHandler}
-              value="adjective" />
+              onChange={this.onChangeHandler}
+              value={adjectiveOne}/>
             <input
               name='nounOne'
               placeholder="noun"
               type="text" 
-              onChange={this.changeHandler}
-              value="noun"/>
+              onChange={this.onChangeHandler}
+              value={nounOne}/>
             <input
               name='adverb'
               placeholder="adverb"
               type="text" 
-              onChange={this.changeHandler}
-              value="adverb"/>
+              onChange={this.onChangeHandler}
+              value={adverb}/>
             <input
               name='verb'
               placeholder="verb"
               type="text" 
-              onChange={this.changeHandler}
-              value="verb"/>
+              onChange={this.onChangeHandler}
+              value={verb}/>
             <input
               name='adjectiveTwo'
               placeholder="adjective"
               type="text" 
-              onChange={this.changeHandler}
-              value="adjective"/>
+              onChange={this.onChangeHandler}
+              value={adjectiveTwo}/>
             <input
               name='nounTwo'
               placeholder="noun"
               type="text" 
-              onChange={this.changeHandler}
-              value="noun"/>
+              onChange={this.onChangeHandler}
+              value={nounTwo}/>
           </div>
 
           <div className="PlayerSubmissionForm__submit">

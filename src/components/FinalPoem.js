927c9f963c55b8cfc0266049fb41ece19f1f947c
Amy Wyatt <amy.wyatt12@outlook.com>
2019-06-18 16:04:04 -0700

    hide and show final poem

--- src/components/FinalPoem.js
+++ src/components/FinalPoem.js
@@ -5,17 +5,17 @@ const FinalPoem = (props) => {
   const poem = props.final.map((line, i) => {
     return (
       <div key={i}>
-        {props.show && line}
+        {line}
       </div>
     );
   });
 
   return (
     <div className="FinalPoem">
-      <section className="FinalPoem__poem">
+      {props.show && <section className="FinalPoem__poem">
         <h3>Final Poem</h3>
         <div>{poem}</div>
-      </section>
+      </section>}
 
       <div className="FinalPoem__reveal-btn-container">
         <input type="button" value="We are finished: Reveal the Poem" className="FinalPoem__reveal-btn" onClick={props.showFinalCallback}/>

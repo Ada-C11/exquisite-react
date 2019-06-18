10b8e7eae38703ca80b5cf57effaf065b3906bdf
Stephanie Marchante <smarcha04@gmail.com>
2019-06-18 14:10:24 -0700

    general scaffolding for player submission form

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -5,7 +5,46 @@ class PlayerSubmissionForm extends Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      adjective: '',
+      noun: '',
+      adverb: '',
+      verb: '',
+      adjective2: '',
+      noun2: '',
+    };
   }
+
+  onChangeHandler = (event) => {
+    const field = {}
+    field[event.target.name] = event.target.value;
+
+    this.setState(field);
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    this.props.addPetCallback({
+      adjective: this.state.adjective,
+      noun: this.state.noun,
+      adverb: this.state.adverb,
+      verb: this.state.verb,
+      adjective2: this.state.adjective2,
+      noun2: this.state.noun2
+    });
+
+    this.setState({
+      adjective: '',
+      noun: '',
+      adverb: '',
+      verb: '',
+      adjective2: '',
+      noun2: '',
+    });
+  }
+ 
 
   render() {
 
@@ -13,16 +52,45 @@ class PlayerSubmissionForm extends Component {
       <div className="PlayerSubmissionForm">
         <h3>Player Submission Form for Player #{  }</h3>
 
-        <form className="PlayerSubmissionForm__form" >
+        <form className="PlayerSubmissionForm__form" onSubmit={this.handleSubmit} >
 
           <div className="PlayerSubmissionForm__poem-inputs">
-
-            {
-              // Put your form inputs here... We've put in one below as an example
-            }
             <input
-              placeholder="hm..."
-              type="text" />
+              placeholder="adjective..."
+              type="text"
+              onChange={this.onChangeHandler}
+              value={this.state.adjective}
+              required />
+            <input
+              placeholder="noun..."
+              type="text" 
+              required
+              onChange={this.onChangeHandler}
+              value={this.state.noun} />
+            <input
+              placeholder="adverb..."
+              type="text"
+              required 
+              onChange={this.onChangeHandler}
+              value={this.state.adverb} />
+            <input
+              placeholder="verb..."
+              type="text" 
+              required 
+              onChange={this.onChangeHandler}
+              value={this.state.verb}/>
+            <input
+              placeholder="adjective..."
+              type="text"
+              required
+              onChange={this.onChangeHandler}
+              value={this.state.adjective} />
+            <input
+              placeholder="noun..."
+              type="text" 
+              required
+              onChange={this.onChangeHandler}
+              value={this.state.noun} />
 
           </div>
 

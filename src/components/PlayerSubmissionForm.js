167786737d854b136b7959ef913f829ae28c38e2
Amy Wyatt <amy.wyatt12@outlook.com>
2019-06-18 14:18:19 -0700

    form fields for playerr submission added

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -5,6 +5,15 @@ class PlayerSubmissionForm extends Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      adjective: '',
+      noun: '',
+      adverb: '',
+      verb: '',
+      adjective2: '',
+      noun2: ''
+    }
   }
 
   render() {
@@ -16,14 +25,57 @@ class PlayerSubmissionForm extends Component {
         <form className="PlayerSubmissionForm__form" >
 
           <div className="PlayerSubmissionForm__poem-inputs">
-
-            {
-              // Put your form inputs here... We've put in one below as an example
-            }
-            <input
-              placeholder="hm..."
-              type="text" />
-
+            <div>
+              The
+            </div>
+            <div>
+              <input
+                name="adjective"
+                onChange={this.onChangeHandler}
+                value={this.state.adjective}
+              />
+            </div>
+            <div>
+              <input
+                name="noun"
+                onChange={this.onChangeHandler}
+                value={this.state.noun}
+              />
+            </div>
+            <div>
+              <input
+                name="adverb"
+                onChange={this.onChangeHandler}
+                value={this.state.adverb}
+              />
+            </div>
+            <div>
+              <input
+                name="verb"
+                onChange={this.onChangeHandler}
+                value={this.state.verb}
+              />
+            </div>
+            <div>
+              the
+            </div>
+            <div>
+              <input
+                name="adjective2"
+                onChange={this.onChangeHandler}
+                value={this.state.adjective2}
+              />
+            </div>
+            <div>
+              <input
+                name="noun2"
+                onChange={this.onChangeHandler}
+                value={this.state.noun2}
+              />
+            </div>
+            <div>
+              .
+            </div>
           </div>
 
           <div className="PlayerSubmissionForm__submit">

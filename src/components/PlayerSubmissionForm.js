18a62f26309c40c92458826a7de4acc2170eca2e
Maria Wissler <wmcarmelina@gmail.com>
2019-06-18 22:07:23 -0700

    install, created fields

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -5,7 +5,44 @@ class PlayerSubmissionForm extends Component {
 
   constructor(props) {
     super(props);
-  }
+   
+    this.state = {
+    adjective: " ",
+    noun: " ",
+    adverb: " ",
+    verb: " ",
+    secondAdjective: "",
+    secondNoun: "",
+  };
+}
+    onWordInput = (event) => {
+      const field = event.target.name;
+      const value = event.target.value; 
+
+      const onUpdateWord = {};
+      onUpdateWord[field]= value;
+      this.setState(onUpdateWord);
+    }
+    
+    onSubmitForm = (event) => {
+      event.preventDefault(); 
+
+      const constructedLine = `The ${this.state.adjective} ${this.state.noun} ${this.state.adverb}
+      ${this.state.verb} ${this.state.secondAdjective} ${this.state.secondNoun}`
+      
+      this.setState = {
+        adjective: " ",
+        noun: " ",
+        adverb: " ",
+        verb: " ",
+        secondAdjective: "",
+        secondNoun: "",
+      };
+
+      console.log("Line:", constructedLine);
+      this.props.addLineCallback(constructedLine);// to do next 
+    }
+
 
   render() {
 
@@ -13,15 +50,49 @@ class PlayerSubmissionForm extends Component {
       <div className="PlayerSubmissionForm">
         <h3>Player Submission Form for Player #{  }</h3>
 
-        <form className="PlayerSubmissionForm__form" >
+        <form className="PlayerSubmissionForm__form" onSubmit={this.onSubmitForm}>
 
           <div className="PlayerSubmissionForm__poem-inputs">
 
             {
               // Put your form inputs here... We've put in one below as an example
             }
+            The
             <input
-              placeholder="hm..."
+              placeholder="Adjective"
+              name="adjective"
+              type="text"
+              onChange={this.onWordInput}
+              type="text" />
+              <input
+              placeholder="Noun"
+              name="noun"
+              type="text"
+              onChange={this.onWordInput}
+              type="text" />
+              <input
+              placeholder="Adverb"
+              name="adverb"
+              type="text"
+              onChange={this.onWordInput}
+              type="text" />
+              <input
+              placeholder="Verb"
+              name="verb"
+              type="text"
+              onChange={this.onWordInput}
+              type="text" />
+              <input
+              placeholder="Adjective"
+              name="secondAdjective"
+              type="text"
+              onChange={this.onWordInput}
+              type="text" />
+              <input
+              placeholder="Noun"
+              name="secondNoun"
+              type="text"
+              onChange={this.onWordInput}
               type="text" />
 
           </div>

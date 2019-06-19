991301fed0ccdb7283a7d65c7e668ca99418c250
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-06-19 14:59:24 -0700

    added proptypes for player submission form

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './PlayerSubmissionForm.css';
+import PropTypes from 'prop-types';
 
 class PlayerSubmissionForm extends Component {
 
@@ -104,6 +105,11 @@ class PlayerSubmissionForm extends Component {
       </div>
     );
   }
-}
+};
+
+PlayerSubmissionForm.propTypes = {
+  onFormSubmitCallback: PropTypes.func,
+  playerNumber: PropTypes.number,
+};
 
 export default PlayerSubmissionForm;

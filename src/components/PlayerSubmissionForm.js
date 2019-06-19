893ee161daad97dff2276efe9ff08f798878c2ff
MyriamWD <myriam.waldend@gmail.com>
2019-06-18 18:06:36 -0700

    added onclick form button  and onChange form input event listeners

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -4,8 +4,54 @@ import './PlayerSubmissionForm.css';
 class PlayerSubmissionForm extends Component {
 
   constructor(props) {
+    
     super(props);
+    this.state = {
+      submission: '',
+      fullSubmission: '',
+      placeholder: '',
+      onFormButtonCallBack: this.props.onPlayerFormButtonCallBack,
+    }
   }
+
+  onChangeInput = (event) => {
+
+    let tempSubmission = ''
+    tempSubmission += (event.target.value)
+
+    this.setState({
+      submission: tempSubmission,
+      placeholder: event.target.placeholder
+    })
+    
+
+    if (this.state.placeholder !== event.target.placeholder) {
+      
+      let insert = this.state.submission;
+      let fullSubmission = this.state.fullSubmission;
+      fullSubmission += insert;
+      this.setState({
+        fullSubmission,
+      })
+      
+    }
+
+  }
+
+  onSubmitForm = (event) => {
+    event.preventDefault();
+  
+    let fullSubmission = this.state.fullSubmission
+    fullSubmission += this.state.submission
+
+    this.setState({
+      fullSubmission,
+    })
+   
+    this.state.onFormButtonCallBack(event, fullSubmission);
+  }
+
+  
 
   render() {
 
@@ -17,17 +63,39 @@ class PlayerSubmissionForm extends Component {
 
           <div className="PlayerSubmissionForm__poem-inputs">
 
-            {
-              // Put your form inputs here... We've put in one below as an example
-            }
-            <input
-              placeholder="hm..."
-              type="text" />
+            
+              The 
 
+              <input
+                placeholder="adjective1"
+                type="text" onChange={this.onChangeInput}/>
+
+              <input
+                placeholder="noun1"
+                type="text" onChange={this.onChangeInput}/>
+              <input
+                placeholder="adverb"
+                type="text" onChange={this.onChangeInput}/>
+              <input
+                placeholder="verb"
+                type="text" onChange={this.onChangeInput}/>
+              
+              the
+
+              <input
+                placeholder="adjective2"
+                type="text" onChange={this.onChangeInput}/>
+
+              <input
+                placeholder="noun2"
+                type="text" onChange={this.onChangeInput}/>
+              
+              .
+            
           </div>
 
           <div className="PlayerSubmissionForm__submit">
-            <input type="submit" value="Submit Line" className="PlayerSubmissionForm__submit-btn" />
+            <input type="submit" value="Submit Line" className="PlayerSubmissionForm__submit-btn" onClick={this.onSubmitForm}/>
           </div>
         </form>
       </div>

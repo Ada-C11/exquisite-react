274fc71bcf5ffb7b1b7b794335550b10e112c96d
Jillianne Ramirez <jillianne.ramirez@gmail.com>
2019-06-21 06:09:49 -0700

    updates form input section to include output if field not an object

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -7,27 +7,45 @@ class PlayerSubmissionForm extends Component {
     super(props);
   }
 
+  formInputs = () => {
+    const fields = this.props.fields.map((field, i) => {
+      if (typeof field !== "object") {
+        return field;
+      } else {
+      return (
+        <div key={i}>
+          <label adjective="fullName">{field.placeholder}</label>
+          <input name="fullName" />
+        </div> 
+      )}
+    });
+
+    return fields
+  }
+
+  // submitLine = () => {
+  //   this.props.onSubmit(line)
+  // }
+
   render() {
 
     return (
       <div className="PlayerSubmissionForm">
-        <h3>Player Submission Form for Player #{  }</h3>
+        <h3>Player Submission Form for Player #{ this.props.player + 1 }</h3>
 
         <form className="PlayerSubmissionForm__form" >
 
           <div className="PlayerSubmissionForm__poem-inputs">
-
-            {
-              // Put your form inputs here... We've put in one below as an example
-            }
-            <input
-              placeholder="hm..."
-              type="text" />
+            
+          <form className="new-student-form">
+            {/* Notes from https://github.com/Ada-Developers-Academy/textbook-curriculum/blob/master/React/forms.md utilized */}
+            {this.formInputs()}
+          </form> 
 
           </div>
 
           <div className="PlayerSubmissionForm__submit">
-            <input type="submit" value="Submit Line" className="PlayerSubmissionForm__submit-btn" />
+            <input onClick={this.submitLine} type="submit" value="Submit Line" className="PlayerSubmissionForm__submit-btn" />
           </div>
         </form>
       </div>
